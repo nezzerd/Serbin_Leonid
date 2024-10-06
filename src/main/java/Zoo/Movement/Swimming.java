6d6870d427932da8aa5f1aa2b1fed3b43c669143
nezzerd <147248331+nezzerd@users.noreply.github.com>
2024-10-07 01:38:39 +0300

    Delete src/main/java/Zoo directory

--- src/main/java/Zoo/Movement/Swimming.java
+++
@@ -1,9 +0,0 @@
-package Zoo.Movement;
-
-import Zoo.Animals.Animal;
-
-public interface Swimming {
-    default void swim() {
-        System.out.println(((Animal) this).getName() + " is swimming.");
-    }
-}
